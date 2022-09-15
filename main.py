3ced1fef3c9b66b96571c464e658e44988468bc1
QÆZZ <65181690+QAEZZ@users.noreply.github.com>
2022-09-15 10:41:48 -0500

    made class and a few methods

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-# ok
+import subprocess as sub
+
+
+def clear(shell=True):
+    sub.call("clear", shell=shell)
+
+
+class Main():
+    
+    def __init__(self):
+        print("ready")
+
+        self.run()
+    
+    def run(self):
+        self.getNums()
+    
+    def getNums(self):
+        print("num1")
+        self.num1 = input("_x_\n>>> ")
+        clear()
+        print("num2")
+        self.num2 = input(f"{self.num1}x_\n>>> ")
+        clear()
+        print("table dimensions")
+        table = f"{self.num1}x{self.num2}"
+        print(table)
+
+
+Main()
